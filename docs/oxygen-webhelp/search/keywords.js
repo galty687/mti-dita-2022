1b832c6a3c30587f3e5550a37b3c52afe96ae7ff
zhijun_gao <gaozhijun@pku.edu.cn>
2022-11-03 11:33:25 +0800

    upload all the files to the github

---
+++ docs/oxygen-webhelp/search/keywords.js
@@ -0,0 +1,8 @@
+var keywords=[{w:"airplay",p:["p0","p1"]},{w:"\u4F7F\u7528",p:["p1"]},{w:"\u8FDB\u884C\u6295\u5F71",p:["p1"]},{w:"\u4F7F\u7528VGA\u5C06Mac\u4E0E\u6295\u5F71\u673A\u76F8\u8FDE",p:["p2"]},{w:"\u6295\u5F71\u4EEA\u53C2\u6570\u4FE1\u606F",p:["p3"]},{w:"\u8FDE\u63A5\u6295\u5F71\u4EEA\u540E\u65E0\u53CD\u6620",p:["p4"]},{w:"VGA\u4ECB\u7ECD",p:["p5"]}];
+var ph={};
+ph["p0"]=[0];
+ph["p1"]=[1, 0, 2];
+ph["p2"]=[3];
+ph["p3"]=[4];
+ph["p4"]=[5];
+ph["p5"]=[6];
